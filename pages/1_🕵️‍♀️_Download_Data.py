6f4028dee11e120779866d8a147d17c31499d8fa
rwxhuang <rwxhuang@gmail.com>
2024-04-24 10:49:26 -0400

    swithc back to chromium - test

--- pages/1_🕵️‍♀️_Download_Data.py
+++ pages/1_🕵️‍♀️_Download_Data.py
@@ -3,12 +3,12 @@ from search import Search
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
 def get_driver():
-    return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))	
+    return webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options)
 
 options = Options()
 options.add_argument("--disable-gpu")
